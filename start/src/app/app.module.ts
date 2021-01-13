e6ff1accbc14a2847c0d178e0fd61d15a3ab9467
AlexanderKovalchuk <alexkovalchuk1991@gmail.com>
2021-01-13 22:24:14 +0200

    start  service  implementation.

--- start/src/app/app.module.ts
+++ start/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +17,8 @@ import { AddUserComponent } from './add-user/add-user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
